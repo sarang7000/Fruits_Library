19045c448733d2727c094ecf0c5745a2638896e9
Sarang Patel <sarangsakariya1999@gmail.com>
2023-08-24 13:58:04 +0530

    Add All my  Files

---
+++ TropicoJs.js
@@ -0,0 +1,12 @@
+
+function locationtop() {
+  location.href = "#top";
+}
+
+// Tap to top Click Color Change
+function mousedown(){
+  document.getElementById("demo").style.color="white";
+} 
+function mouseup(x){
+  document.getElementById("demo").style.color="black";	
+}
